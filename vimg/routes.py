b98461802433fceb303a6ee206acf81d4cccaa4a
Cledersonbc <cledersonbc@live.com>
2025-01-13 00:49:48 -0300

    added logic to forms

--- vimg/routes.py
+++ vimg/routes.py
@@ -1,14 +1,18 @@
 from vimg import app
 from flask import render_template
 
-@app.route("/")
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
 def login():
     return render_template('login.html')
 
-@app.route("/signup")
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
     return render_template('signup.html')
 
-@app.route("/upload")
+@app.route('/upload')
 def upload():
     return render_template('upload.html')
