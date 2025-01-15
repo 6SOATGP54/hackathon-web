7148db512a084dbc5d0cd7d41abf207cc9c8c44b
Cledersonbc <cledersonbc@live.com>
2025-01-15 02:19:43 -0300

    added login service

--- vimg/routes.py
+++ vimg/routes.py
@@ -1,9 +1,8 @@
-from vimg.models import User
-from flask.helpers import flash, redirect, url_for
+from flask.helpers import redirect, url_for
 from vimg.services import UserService
 from vimg import app
 from flask import render_template, request
-from flask_login import login_required, login_user, logout_user
+from flask_login import login_required, logout_user
 
 @app.route('/', methods=['GET'])
 def home():
@@ -11,18 +10,7 @@ def home():
 
 @app.route('/login', methods=['POST'])
 def login():
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-
-        found_user = User.query.filter_by(email=email).first()
-
-        if found_user and found_user.verify_password(password):
-            login_user(found_user)
-            flash(found_user.first_name, 'success')
-            return redirect(url_for('upload'))
-        else:
-            return render_template('login.html')
+    return UserService.login_and_redirect(request)
 
 @app.route('/logout', methods=['GET'])
 def logout():
@@ -34,7 +22,7 @@ def signup():
     if request.method == 'GET':
         return render_template('signup.html')
     else:
-        return UserService.save(request)
+        return UserService.save_and_redirect(request)
 
 @app.route('/upload')
 @login_required
--- vimg/services.py
+++ vimg/services.py
@@ -1,11 +1,12 @@
 from flask import redirect, flash
 from flask.helpers import url_for
+from flask_login.utils import login_user
 from vimg.models import User
 from vimg import db
 
 class UserService:
     
-    def save(request):
+    def save_and_redirect(request):
         first_name = request.form['first_name']
         last_name = request.form['last_name']
         email = request.form['email']
@@ -23,4 +24,18 @@ class UserService:
             return redirect(url_for('home'))
         else:
             flash('Erro! As informações fornecidas não são válidas.', 'error')
+            return redirect(url_for('home'))
+
+    def login_and_redirect(request):
+        email = request.form['email']
+        password = request.form['password']
+
+        found_user = User.query.filter_by(email=email).first()
+
+        if found_user and found_user.verify_password(password):
+            login_user(found_user)
+            flash(found_user.first_name, 'success')
+            return redirect(url_for('upload'))
+        else:
+            flash('Houve um problema com seu usuário. Por favor, entre em contato com o suporte.', 'error')
             return redirect(url_for('home'))
