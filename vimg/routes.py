c14a0f6d78a6ef672277e8c2bf4dc84369e76787
Cledersonbc <cledersonbc@live.com>
2025-01-16 00:35:53 -0300

    added error handler to routes

--- vimg/routes.py
+++ vimg/routes.py
@@ -1,6 +1,7 @@
 from flask.helpers import redirect, url_for
 from vimg.services import UserService
 from vimg import app
+from vimg import login_manager
 from flask import render_template, request
 from flask_login import login_required, logout_user
 
@@ -28,3 +29,15 @@ def signup():
 @login_required
 def upload():
     return render_template('upload.html')
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    return render_template('errors/401.html')
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('errors/404.html')
+
+@app.errorhandler(Exception)
+def generic_error(e):
+    return render_template('errors/generic.html')
