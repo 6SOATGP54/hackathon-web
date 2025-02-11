007379cb305464d58a3a0eb7d584c946947cf4e6
Cledersonbc <cledersonbc@live.com>
2025-02-10 22:36:07 -0300

    added tables

--- vimg/__init__.py
+++ vimg/__init__.py
@@ -3,6 +3,7 @@ from vimg.models import User
 from vimg.models import db, login_manager, bcrypt
 from flask import Flask
 import logging
+import os
 
 logging.basicConfig(
     format='%(asctime)s %(levelname)-8s %(message)s',
@@ -11,7 +12,7 @@ logging.basicConfig(
 )
 app = Flask(__name__)
 app.secret_key = uuid.uuid4().hex
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://trois:V13rg3s@localhost/vimg'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
 
 db.init_app(app)
 bcrypt.init_app(app)
